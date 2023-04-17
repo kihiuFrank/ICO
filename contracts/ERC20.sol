ee3b7efa6c5536b02fe8c4483bdafbe4562ab6af
kihiuFrank <franklinekihiu@gmail.com>
2023-04-18 01:51:34 +0300

    contracts

---
+++ contracts/ERC20.sol
@@ -0,0 +1,169 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface ERC20Interface {
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `recipient`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(
+        address owner,
+        address spender
+    ) external view returns (uint256);
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Moves `amount` tokens from `sender` to `recipient` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
+}
+
+contract Block is ERC20Interface {
+    // state variables
+    string public name = "Block";
+    string public symbol = "BLK";
+    uint public decimal = 0;
+    uint public override totalSupply;
+    address public founder;
+
+    mapping(address => uint) balances;
+    mapping(address => mapping(address => uint)) allowed;
+
+    constructor() {
+        totalSupply = 100000;
+        founder = msg.sender;
+        balances[founder] = totalSupply;
+    }
+
+    function balanceOf(
+        address tokenOwner
+    ) public view override returns (uint256) {
+        return balances[tokenOwner];
+    }
+
+    function transfer(
+        address _to,
+        uint256 tokens
+    ) public override returns (bool) {
+        // first check if they have enough tokens in their account
+        require(
+            balances[msg.sender] >= tokens,
+            "Not enough tokens to transfer"
+        );
+        //transfering tokens
+        balances[_to] += tokens;
+        //subtract tokens from sender
+        balances[msg.sender] -= tokens;
+        emit Transfer(msg.sender, _to, tokens);
+        return true;
+    }
+
+    function allowance(
+        address owner,
+        address spender
+    ) external view override returns (uint256) {
+        return allowed[owner][spender];
+    }
+
+    function approve(
+        address spender,
+        uint256 tokens
+    ) external override returns (bool) {
+        // first check if the owner has enough tokens in their account
+        require(
+            balances[msg.sender] >= tokens,
+            "Not enough tokens to transfer"
+        );
+
+        // check tokens being transfered are greater than 0
+        require(tokens > 0, "token must be greater than 0");
+        allowed[msg.sender][spender] = tokens;
+        emit Transfer(msg.sender, spender, tokens);
+        return true;
+    }
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 tokens
+    ) external override returns (bool) {
+        require(allowed[sender][recipient] >= tokens);
+        require(
+            balances[sender] >= tokens,
+            "you cannot transfer more than you have"
+        );
+
+        // transfering
+        balances[sender] -= tokens;
+        balances[recipient] += tokens;
+        return true;
+    }
+}
--- contracts/ICO.sol
+++ contracts/ICO.sol
@@ -1,7 +1,90 @@
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-contract BlockToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Block", "BLK") {
-        _mint(msg.sender, initialSupply);
+import "./ERC20.sol";
+
+contract ICO is Block {
+    address public manager;
+    address payable public deposit;
+    uint tokenPrice = 0.1 ether;
+
+    uint public cap = 300 ether; // amount of ether we want in circulation
+    uint public raisedAmount;
+    uint public icoStart = block.timestamp; // ICO starts when we deploy
+    uint public icoEnd = block.timestamp + 3600; // ICO to run for 1hr (60*60 secs)
+    uint tokenTradeTime = icoEnd + 3600; // You can trade the tokens after 1hr of the ICO's end.abi
+
+    //Prevent people from selling ether when the ICO is running (avoiding losses)
+
+    uint public maxInvest = 10 ether; // max amount an invetor can invest
+    uint public minInvest = 0.1 ether; // min amount an investor can invest.abi
+
+    enum State {
+        beforeStart,
+        afterEnd,
+        running,
+        halted
+    }
+
+    State public icoState;
+
+    event Invest(address investor, uint value, uint tokens);
+
+    constructor(address payable _deposit) {
+        deposit = _deposit;
+        manager = msg.sender;
+        icoState = State.beforeStart;
+    }
+
+    modifier onlyManager() {
+        require(msg.sender == manager);
+        _;
+    }
+
+    function halt() public onlyManager {
+        icoState = State.halted;
+    }
+
+    function resume() public onlyManager {
+        icoState = State.running;
+    }
+
+    // as a security feature we need option to change deposit address incase old one got hacked (preventing further losses)
+    function changeDepositAddr(address payable newDeposit) public onlyManager {
+        deposit = newDeposit;
+    }
+
+    function invest() public payable returns (bool) {
+        icoState = getState();
+        require(
+            icoState == State.running,
+            "cannot invest when ICO isn't running"
+        );
+
+        require(
+            msg.value >= minInvest && msg.value <= maxInvest,
+            "your amount must be within allowed range"
+        );
+
+        raisedAmount = msg.value;
+
+        require(raisedAmount <= cap, "amount raised cannot exceed the set cap");
+
+        uint tokens = msg.value / tokenPrice; // if we invest 10 ether we get (10/0.1) tokens
+
+        // update balances
+        balances[msg.sender] += tokens;
+    }
+
+    function getState() public view returns (State) {
+        if (icoState == State.halted) {
+            return State.halted;
+        } else if (block.timestamp < icoStart) {
+            return State.beforeStart;
+        } else if (block.timestamp > icoStart && block.timestamp < icoEnd) {
+            return State.running;
+        } else {
+            return State.afterEnd;
+        }
     }
 }
